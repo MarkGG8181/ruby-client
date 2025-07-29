98031d7a7e30d1a819f61b1eeab367c87906cf7c
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-07-29 20:44:55 +0300

    clickgui recode

--- src/main/java/io/github/ruby/module/impl/render/ClickGuiModule.java
+++ src/main/java/io/github/ruby/module/impl/render/ClickGuiModule.java
@@ -3,9 +3,7 @@ package io.github.ruby.module.impl.render;
 import io.github.ruby.module.AbstractModule;
 import io.github.ruby.module.ModuleCategory;
 import io.github.ruby.module.ModuleInfo;
-import io.github.ruby.processor.impl.ScreenProcessor;
 import io.github.ruby.screen.impl.ClickGuiScreen;
-import io.github.ruby.storage.impl.ProcessorStorage;
 import org.lwjgl.input.Keyboard;
 
 @ModuleInfo(name = "ClickGui", description = "Renders a clicky gui.", category = ModuleCategory.RENDER)
@@ -22,7 +20,7 @@ public class ClickGuiModule extends AbstractModule {
             clickGuiScreen = new ClickGuiScreen();
         }
 
-        ProcessorStorage.INSTANCE.getByClass(ScreenProcessor.class).setCurrentScreen(clickGuiScreen);
+        mc.displayGuiScreen(clickGuiScreen);
         setEnabled(false);
     }
 }
--- src/main/java/io/github/ruby/screen/impl/ClickGuiScreen.java
+++ src/main/java/io/github/ruby/screen/impl/ClickGuiScreen.java
@@ -1,12 +1,8 @@
 package io.github.ruby.screen.impl;
 
-import io.github.ruby.module.AbstractModule;
 import io.github.ruby.module.ModuleCategory;
 import io.github.ruby.screen.AbstractScreen;
-import io.github.ruby.screen.element.impl.ButtonElement;
-import io.github.ruby.screen.element.impl.TextElement;
-import io.github.ruby.screen.element.option.TextAlignment;
-import io.github.ruby.storage.impl.ModuleStorage;
+import io.github.ruby.util.game.shader.ShaderUtil;
 import org.lwjgl.util.vector.Vector2f;
 
 import java.util.HashMap;
@@ -15,44 +11,32 @@ import java.util.Map;
 public class ClickGuiScreen extends AbstractScreen {
     private final HashMap<ModuleCategory, Vector2f> positions = new HashMap<>();
 
+    private static final Vector2f size = new Vector2f(100, 16);
+
     public ClickGuiScreen() {
         super("Click Gui");
-
-        float offset = 5;
-        for (ModuleCategory value : ModuleCategory.values()) {
-            positions.put(value, new Vector2f(offset, 5));
-            offset += 80 + 4;
-        }
     }
 
     @Override
     public void onInit() {
         super.onInit();
-        for (Map.Entry<ModuleCategory, Vector2f> entry : positions.entrySet()) {
-            children.add(TextElement.builder()
-                    .text(entry.getKey().toString())
-                    .position(entry.getValue().getX(), entry.getValue().getY())
-                    .textAlignment(TextAlignment.CENTER)
-                    .background(true)
-                    .build(this)
-            );
 
-            float offset = entry.getValue().getY() + 13.5f;
-            for (AbstractModule module : ModuleStorage.INSTANCE.getByCategory(entry.getKey())) {
-                children.add(ButtonElement.builder()
-                        .text(module.getName())
-                        .textAlignment(TextAlignment.CENTER_LEFT)
-                        .position(entry.getValue().getX(), offset)
-                        .onClick(() -> module.setEnabled(!module.isEnabled()))
-                        .build(this)
-                );
-                offset += 14;
-            }
+        positions.clear();
+
+        var offset = 5f;
+        for (ModuleCategory category : ModuleCategory.values()) {
+            positions.put(category, new Vector2f(offset, 5));
+            offset += size.x + 5;
         }
     }
 
     @Override
     public void onRender(int mouseX, int mouseY, float partialTicks) {
-        children.forEach(c -> c.onRender(mouseX, mouseY, partialTicks));
+        for (Map.Entry<ModuleCategory, Vector2f> entry : positions.entrySet()) {
+            ModuleCategory category = entry.getKey();
+            Vector2f position = entry.getValue();
+
+            ShaderUtil.rect(position.x, position.y, size.x, size.y, 5, BACKGROUND);
+        }
     }
 }
